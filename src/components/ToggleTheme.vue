82d1dbe4ce405b4813ee89edd74f5089352d1d56
Ayoub Rbahi <rbahiayoub@gmail.com>
2024-11-25 08:57:40 +0800

    Update ToggleTheme.vue

--- src/components/ToggleTheme.vue
+++ src/components/ToggleTheme.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watchEffect } from "vue";
+import { watchEffect } from "vue";
 import { useColorMode } from "@vueuse/core";
 import { Button } from "@/components/ui/button";
 import { Moon, Sun } from "lucide-vue-next";
