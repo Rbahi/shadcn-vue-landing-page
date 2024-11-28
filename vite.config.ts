7b60bf7fdcce7782c709edd4711d6b546f715702
Ayoub Rbahi <rbahiayoub@gmail.com>
2024-11-28 20:54:57 +0800

    sitemap added

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import Sitemap from "vite-plugin-sitemap";
 
 import tailwind from "tailwindcss";
 import autoprefixer from "autoprefixer";
@@ -11,7 +12,16 @@ export default defineConfig({
       plugins: [tailwind(), autoprefixer()],
     },
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Sitemap({
+      hostname: "https://fmitradinggroup.com", // Your base URL
+      outDir: "dist", // Output directory for generated files
+      extensions: "html", // Extensions to include in the sitemap (e.g., HTML)
+      generateRobotsTxt: true, // Whether to generate a robots.txt
+      robots: [{ userAgent: "*", allow: "/" }], // Allow all user agents to crawl the site
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
